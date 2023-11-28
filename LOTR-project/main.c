0099df80ef5d578e2ca473cd02945559d1198f0f
Jean Oliveira <jcwilbertt@gmail.com>
2023-11-28 16:00:45 +0000

    first commit

--- LOTR-project/main.c
+++ LOTR-project/main.c
@@ -1,81 +1,34 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NO_SECURE_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
-int tabuleiro()
-{
-	printf("   A B C D E F G H I J K L M N O P Q R J T U V W X Y Z\n");
-	//NOMES DAS COLUNAS
-	printf("  %c", 218);
-	for (int i = 0; i < 25; i++)
-	{
-		printf("%c%c", 196, 194);
-	}
-	printf("%c%c\n", 196, 191);
-	//PARTE SUPERIOR DA TABELA
-	for (int ii = 0; ii < 10; ii++)
-	{
-		printf("%d ", ii);
-		for (int i = 0; i < 27; i++)
-		{
-			printf("%c ", 179);
-		}
-		printf("\n  %c", 195);
-		for (int i = 0; i < 25; i++)
-		{
-			printf("%c%c", 196, 197);
-		}
-		printf("%c%c\n", 196, 180);
-	}
-	//TABELA ATÉ A LINHA 9
-	for (int ii = 10; ii < 16; ii++)
-	{
-		printf("%d", ii);
-		for (int i = 0; i < 27; i++)
-		{
-			printf("%c ", 179);
-		}
-		printf("\n  %c", 195);
-		for (int i = 0; i < 25; i++)
-		{
-			printf("%c%c", 196, 197);
-		}
-		printf("%c%c\n", 196, 180);
-	}
-	//TABELA DA LINHA 10 A 16 - DOIS DIGITOS = MENOS UM ESPAÇO (PARA A TABELA FICAR ALINHADA;CONFIA)
-	printf("16");
-	for (int i = 0; i < 27; i++)
-	{
-		printf("%c ", 179);
-	}
-	//FAZ A ULTIMA LINHA DA TABELA
-	printf("\n  %c", 192);
-	for (int i = 0; i < 25; i++)
-	{
-		printf("%c%c", 196, 193);
-	}
-	printf("%c%c", 196, 217);
-	//PARTE INFERIOR DA TABELA
+#define LINHAS 17
+#define COLUNAS 26
+
+// Estrutura do tabuleiro
+char tabuleiro[LINHAS][COLUNAS];
+
+void menu() {
+    printf("(1) New game\n(2) Load game\n(3) Settings\n(4) Exit");
 }
 
-int main()
-{
-	int opcao = 0;
-	while (opcao != 4)
-	{
-		printf("bem-vindo blablabla\nSelecione uma opcao:\n\n");
-		printf("1. Novo jogo\n2. Carregar jogo\n3. Configuracoes\n4. Sair\n");
-		scanf("%d", &opcao);
-		if (opcao == 1)
-		{
-			tabuleiro();
-		}
-		else if (opcao == 2)
-		{
+int main() {
+    menu();
+    
+    int option;
+    scanf("%d", &option);
+    switch (option) {
+        case 1:
+            system("cls");
+            inicializarTabuleiro();
+            imprimirTabuleiro();
+        case 2:
 
-		}
-		else if (opcao == 3)
-		{
-			printf("1. Idioma\n2. Creditos\n3. ");
-		}
-	}
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+    }
 }
