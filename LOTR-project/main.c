92a3cc5c3bbb2db54e780c2ec0a9704a08af624c
Jean Oliveira <jcwilbertt@gmail.com>
2023-11-28 16:58:57 +0000

    initializeBoard

--- LOTR-project/main.c
+++ LOTR-project/main.c
@@ -12,6 +12,34 @@ void menu() {
     printf("(1) New game\n(2) Load game\n(3) Settings\n(4) Exit");
 }
 
+// Função para inicializar o tabuleiro (preenchendo com espaços em branco)
+void inicializarTabuleiro() {
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            tabuleiro[i][j] = ' ';
+        }
+    }
+}
+
+// Função para imprimir o tabuleiro
+void imprimirTabuleiro() {
+    printf("  ");
+    for (int j = 0; j < COLUNAS; j++) {
+        printf("  %c", 'A' + j);
+    }
+    printf("\n");
+
+    for (int i = 0; i < LINHAS; i++) {
+        printf("%2d ", i + 1);
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("[");
+            printf(" %c ", tabuleiro[i][j]);
+            printf("]");
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     menu();
     
